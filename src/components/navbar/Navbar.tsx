7321235a623040c5de20916458006f7849857e78
julien willette <julien.willette@gmail.com>
2021-04-27 16:02:24 +0200

    add icon

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import { Navigation } from "react-minimal-side-navigation";
 import "react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css";
 import { Link } from "react-router-dom";
+import { BsFillGearFill } from "react-icons/bs";
+import { TiGroup } from "react-icons/ti";
+import { ImBooks } from "react-icons/im";
+import { MdSchool } from "react-icons/md";
+import { AiFillStar } from "react-icons/ai";
+import { GoTasklist } from "react-icons/go";
 import { AllNav } from "../../assets/styles/navbar/Navbar";
 import logo from "../../assets/pictures/logo.png";
 import profile from "../../assets/pictures/profile.png";
-import { BsFillGearFill } from "react-icons/fa";
 
 export const Navbar = () => {
   return (
@@ -22,6 +27,7 @@ export const Navbar = () => {
         }}
         items={[
           {
+            elemBefore: () => <TiGroup />,
             title: "Campus de Paris",
             itemId: "/Paris",
           },
@@ -31,12 +37,24 @@ export const Navbar = () => {
             itemId: "/Dashboard",
           },
           {
+            elemBefore: () => <ImBooks />,
             title: "Ressources",
             itemId: "/Ressources",
           },
           {
+            elemBefore: () => <MdSchool />,
             title: "Cours",
             itemId: "/Cours",
+          },
+          {
+            elemBefore: () => <AiFillStar />,
+            title: "Favoris",
+            itemId: "/Favoris",
+          },
+          {
+            elemBefore: () => <GoTasklist />,
+            title: "Bilan compétences",
+            itemId: "/Bilan",
           },
         ]}
       />
