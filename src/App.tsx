6f429bf4b33e57c083892f7ccecf943c81b2aefe
julien willette <julien.willette@gmail.com>
2021-04-29 15:59:05 +0200

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { Navbar } from "./components/navbar/Navbar";
+import { Sidebar } from "./components/sidebar/Sidebar";
 import ModuleList from "./components/course/students/ModuleList";
 import "./App.css";
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <Navbar />
+      <Sidebar />
       <ModuleList />
     </div>
   );
---
+++ src/assets/styles/sidebar/Sidebar.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { colors, fonts } from "../globals";
+
+export const AllSidebar = styled.nav`
+  background-color: ${colors.secondary};
+`;
