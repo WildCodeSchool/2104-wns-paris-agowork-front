c65b3ef0f98e0ac75e60f0a7c01675b7502aadcc
Oceane-Kolau <Oceane-Kolau@users.noreply.github.com>
2021-12-01 11:30:49 +0100

    modified logic to have login page without navbar, add flexbox for content when nav is open

--- src/routing/Router.tsx
+++ src/routing/Router.tsx
@@ -1,32 +1,23 @@
-// eslint-disable-next-line
-// @ts-nocheck
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import UserCreation from "../pages/admin/User_Creation";
-import Dashboard from "../pages/public/Dashboard";
-import Login from "../pages/public/Login";
-import ModuleList from "../components/course/students/Module_List";
-import Ressources from "../components/ressources/Ressources";
+import Box from "@mui/material/Box";
+import { useLocation } from "react-router-dom";
+
+import Page from "./Page";
 import Sidebar from "../components/sidebar/Sidebar";
-import { Container, ModuleWrapper } from "../assets/styles/course/Elements";
-import AuthRoute from "./AuthRoute";
 
 const Router = (): JSX.Element => {
   return (
     <>
-      <Sidebar />
-      <Container>
-        <ModuleWrapper>
-          <Switch>
-      
-            <AuthRoute exact path="/" component={Dashboard} />
-            <AuthRoute exact path="/ressources" component={Ressources} />
-            <AuthRoute exact path="/modules" component={ModuleList} />
-            <AuthRoute exact path="/creation-user" component={UserCreation} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </ModuleWrapper>
-      </Container>
+      {useLocation().pathname !== "/login" ? (
+        <Box sx={{ display: "flex" }}>
+          <Sidebar />
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <Page />
+          </Box>
+        </Box>
+      ) : (
+        <Page />
+      )}
     </>
   );
 };
