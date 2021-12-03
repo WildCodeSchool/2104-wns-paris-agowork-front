c1f032d9819b499150db886e334cac2dff45dde6
Oceane-Kolau <Oceane-Kolau@users.noreply.github.com>
2021-12-03 15:13:30 +0100

    update

--- src/pages/admin/userCreation.tsx
+++ src/pages/admin/userCreation.tsx
@@ -8,7 +8,7 @@ import {
   Select,
   SelectChangeEvent,
 } from "@mui/material";
-import { CreateUser } from "../../graphql/mutations/user/user";
+import { CreateUser } from "../../graphql/mutations/user/user1";
 import { StyledButton } from "../../assets/styles/course/elements";
 import { Form } from "../../assets/styles/global";
 
